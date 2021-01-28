f07448dd0bde4f20d97202248ae770d33797237c
Pratish7 <pratishbajpai7@gmail.com>
2021-01-28 16:19:20 +0530

    working on stream fix

--- stream.java
+++ stream.java
@@ -8,9 +8,9 @@ import com.netsdk.lib.ToolKits;
 import com.sun.jna.Native;
 import java.awt.GridLayout;
 
-public class stream {
-    public static JFrame frame;
-    public static Panel panel;	
+
+public class stream {	
+
 	public static LLong startRealPlay(int channel, int stream, Panel realPlayWindow) {
 		LLong m_hPlayHandle = loginModule.netsdk.CLIENT_RealPlayEx(loginModule.m_hLoginHandle, channel, Native.getComponentPointer(realPlayWindow), stream);
 	
@@ -23,30 +23,25 @@ public class stream {
 	    return m_hPlayHandle;
 	} 
 	
-	/**
-	 * \if ENGLISH_LANG
-	 * Start RealPlay
-	 * \else
-	 * 停止预览
-	 * \endif
-	 */
-	public static void stopRealPlay(LLong m_hPlayHandle) {
-		if(m_hPlayHandle.longValue() == 0) {
-			return;
-		}
+	
+	// public static void stopRealPlay(LLong m_hPlayHandle) {
+	// 	if(m_hPlayHandle.longValue() == 0) {
+	// 		return;
+	// 	}
 		
-		boolean bRet = loginModule.netsdk.CLIENT_StopRealPlayEx(m_hPlayHandle);
-		if(bRet) {
-			m_hPlayHandle.setValue(0);
-		}
-	}
+	// 	boolean bRet = loginModule.netsdk.CLIENT_StopRealPlayEx(m_hPlayHandle);
+	// 	if(bRet) {
+	// 		m_hPlayHandle.setValue(0);
+	// 	}
+	// }
+
 public static void main(String args[]){
 	init_.init(null, null);
 	loginModule.login("192.168.1.108", 37777, "admin", "admin@123");
-	frame = new JFrame();
+	JFrame frame = new JFrame();
     frame.setSize(500,500);
     frame.setLayout(new GridLayout(1,1));
-    panel = new Panel();
+    Panel panel = new Panel();
     panel.setBackground(Color.BLACK);
     frame.add(panel);
 	panel.setVisible(true);
